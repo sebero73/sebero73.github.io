174bcf48becb598703ca731d83506a6079d5ca8a
steven73 <steven@weconnected.kr>
2022-08-15 20:43:30 +0900

    web foler

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6373869876b09335a049b2333315b448",
-"index.html": "976486582be0580776d1f988284f8b19",
-"/": "976486582be0580776d1f988284f8b19",
-"main.dart.js": "9390dd2c7917fd93469cadb8bf5f55d5",
+"index.html": "6dc150975c70b3908256ce27ef8b915d",
+"/": "6dc150975c70b3908256ce27ef8b915d",
+"main.dart.js": "f6ac45e2556048692b87b072ab659d03",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
